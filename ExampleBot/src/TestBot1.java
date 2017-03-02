7aebac9f62b14fc7592811d873963817ab07fc70
RaeveSpam <viking.buhl@gmail.com>
2017-03-03 00:56:43 +0100

    + naive supply management + naive build placement

--- ExampleBot/src/TestBot1.java
+++ ExampleBot/src/TestBot1.java
@@ -50,10 +50,28 @@ public class TestBot1 extends DefaultBWListener {
 
         StringBuilder units = new StringBuilder("My units:\n");
 
+      //if we're running out of supply and have enough minerals ...
+        if ((self.supplyTotal() - self.supplyUsed() < 2) && (self.minerals() >= 100)) {
+        	//iterate over units to find a worker
+        	for (Unit myUnit : self.getUnits()) {
+        		if (myUnit.getType() == UnitType.Terran_SCV) {
+        			//get a nice place to build a supply depot
+        			TilePosition buildTile =
+        				getBuildTile(myUnit, UnitType.Terran_Supply_Depot, self.getStartLocation());
+        			//and, if found, send the worker to build it (and leave others alone - break;)
+        			if (buildTile != null) {
+        				myUnit.build(UnitType.Terran_Supply_Depot, buildTile);
+        				break;
+        			}
+        		}
+        	}
+        }
+        
         //iterate through my units
         for (Unit myUnit : self.getUnits()) {
             units.append(myUnit.getType()).append(" ").append(myUnit.getTilePosition()).append("\n");
 
+          
             //if there's enough minerals, train an SCV
             if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50) {
                 myUnit.train(UnitType.Terran_SCV);
@@ -79,10 +97,63 @@ public class TestBot1 extends DefaultBWListener {
             }
         }
 
+      
+        
         //draw my units on screen
         game.drawTextScreen(10, 25, units.toString());
     }
 
+ // Returns a suitable TilePosition to build a given building type near
+ // specified TilePosition aroundTile, or null if not found. (builder parameter is our worker)
+ public TilePosition getBuildTile(Unit builder, UnitType buildingType, TilePosition aroundTile) {
+ 	TilePosition ret = null;
+ 	int maxDist = 3;
+ 	int stopDist = 40;
+
+ 	// Refinery, Assimilator, Extractor
+ 	if (buildingType.isRefinery()) {
+ 		for (Unit n : game.neutral().getUnits()) {
+ 			if ((n.getType() == UnitType.Resource_Vespene_Geyser) &&
+ 					( Math.abs(n.getTilePosition().getX() - aroundTile.getX()) < stopDist ) &&
+ 					( Math.abs(n.getTilePosition().getY() - aroundTile.getY()) < stopDist )
+ 					) return n.getTilePosition();
+ 		}
+ 	}
+
+ 	while ((maxDist < stopDist) && (ret == null)) {
+ 		for (int i=aroundTile.getX()-maxDist; i<=aroundTile.getX()+maxDist; i++) {
+ 			for (int j=aroundTile.getY()-maxDist; j<=aroundTile.getY()+maxDist; j++) {
+ 				if (game.canBuildHere(new TilePosition(i,j), buildingType, builder, false)) {
+ 					// units that are blocking the tile
+ 					boolean unitsInWay = false;
+ 					for (Unit u : game.getAllUnits()) {
+ 						if (u.getID() == builder.getID()) continue;
+ 						if ((Math.abs(u.getTilePosition().getX()-i) < 4) && (Math.abs(u.getTilePosition().getY()-j) < 4)) unitsInWay = true;
+ 					}
+ 					if (!unitsInWay) {
+ 						return new TilePosition(i, j);
+ 					}
+ 					// creep for Zerg
+ 					if (buildingType.requiresCreep()) {
+ 						boolean creepMissing = false;
+ 						for (int k=i; k<=i+buildingType.tileWidth(); k++) {
+ 							for (int l=j; l<=j+buildingType.tileHeight(); l++) {
+ 								if (!game.hasCreep(k, l)) creepMissing = true;
+ 								break;
+ 							}
+ 						}
+ 						if (creepMissing) continue;
+ 					}
+ 				}
+ 			}
+ 		}
+ 		maxDist += 2;
+ 	}
+
+ 	if (ret == null) game.printf("Unable to find suitable build position for "+buildingType.toString());
+ 	return ret;
+ }
+    
     public static void main(String[] args) {
         new TestBot1().run();
     }
